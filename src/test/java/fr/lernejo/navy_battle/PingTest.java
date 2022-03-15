b9aeda7221278cdd0058f6b39fd68daeb45a167b
Hanarion <landryjuge@gmail.com>
2022-03-15 19:43:36 +0100

    Creating Ping test

---
+++ src/test/java/fr/lernejo/navy_battle/PingTest.java
@@ -0,0 +1,32 @@
+package fr.lernejo.navy_battle;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class PingTest {
+    @org.junit.jupiter.api.Test
+    void simpleGetPing() throws IOException {
+        String[] args = {"18596"};
+        Launcher.main(args);
+
+        URL url = new URL("http://localhost:18596/ping");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        Assertions.assertEquals(status, 200, "Le statut HTTP n'est pas valide");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+
+        StringBuilder content = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        Assertions.assertEquals(content.toString(), "OK", "Le message n'est pas valide");
+    }
+}
